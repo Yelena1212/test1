7e5f488c19f40653ba92faaec1aa0071943692ac
Yelena Baranchuk <yelenabay@Yelenas-MacBook-Pro.local>
2020-05-14 22:13:14 -0700

    Initial commit

---
+++ test/index.spec.js
@@ -0,0 +1,29 @@
+const { add, mult } = require('../helpers');
+const assert = require('assert')
+// const expect = require('chai').expect;
+it('should  add 2 + 2 eq 4', () => {
+
+});
+
+it('should function add 2+ 2 = 4', () =>{
+    assert.equal(add (2, 5), 7)
+} )
+
+
+it('should function add 20 - 2', () =>{
+    assert.equal(add (20, -2), 18)
+} )
+
+
+// console.log("Hello QA6")
+// console.log(add( 45,54));
+// console.log(mult(45, 54));
+//
+//
+// const r1 = add(2,2)
+//
+// if (r1 === 4){
+//     console.log('add - ok')
+// } else {
+//     console.log('add - wrong')
+// }
